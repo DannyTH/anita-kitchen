d82ae39ab999e37524f630dfd3c163c67b670bd9
dannyTH <dannymaish80@gmail.com>
2018-04-27 16:40:07 +0300

    Add changes made

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("button#food").click(function() {
     $("#initially-hidden").slideToggle();
+    $("button#food").removeClass();
+    $("button#food").addClass("white");
   });
 });
