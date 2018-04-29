a24deb52a50c773c626c2fc8331db333ee27a62c
dannyTH <dannymaish80@gmail.com>
2018-04-29 21:23:45 +0300

    Add changes made

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
     $("button#food").removeClass();
     $("button#food").addClass("white");
   });
+  $("#blanks form").submit(function(event) {
+     var person1Input = $("input#person1").val();
+     $(".person1").alert("congratulations")
+
+    event.preventDefault();
+  });
 });
